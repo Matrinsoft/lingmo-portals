43e88f0703ae479e45f26f7c751c2167c70cde60
Lingmo <lingmo@lingmo.org>
2026-07-10 19:04:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+﻿# lingmo-portals
+
+Lingmo Portals
+
+## Build
+
+`ash
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+make -j
+`
+
+## Dependencies
+
+- Qt6 >= 6.5
+- C++20
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <QtCore/qglobal.h>
+
+#if defined(LINGMO_LINGMO_PORTALS_LIBRARY)
+#  define LINGMO_LINGMO_PORTALS_EXPORT Q_DECL_EXPORT
+#else
+#  define LINGMO_LINGMO_PORTALS_EXPORT Q_DECL_IMPORT
+#endif
